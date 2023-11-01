c25ebe08063cf5af42c8e40de24b9e60b8c88d2e
sseung <dptmejf34@gmail.com>
2023-11-02 07:46:44 +0900

    work

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,4 +1,20 @@
 /* common */
 $(function(){
-
+    accordionFn();
 });
+
+function accordionFn(){
+    $('.questions .quest-btn').on('click', function() {
+
+        $('.questions .quest-btn').removeClass('active');
+        $('.questions .txt-box').hide();
+
+        if(!$(this).hasClass('active')) {
+            $(this).addClass('active');
+            $(this).siblings('.txt-box').stop().show();
+        } else {
+            $(this).removeClass('active');
+            $(this).siblings('.txt-box').stop().hide();
+        }
+    });
+}
